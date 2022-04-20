1592afa0b276a77a07dcd3446d435052063e4204
Navin Patil <navinpatilwork@gmail.com>
2022-04-20 10:09:10 +0530

    Enter

---
+++ authentication/urls.py
@@ -0,0 +1,20 @@
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('',views.home, name = "home"),
+    path('signup',views.signup, name="signup"),
+    path('signin',views.signin, name="signin"),
+    path('signout',views.signout, name="signout"),
+    path('aboutus',views.aboutus, name="aboutus"),
+    path('std10',views.std10, name="std10"),
+    path('std11',views.std11, name="std11"),
+    path('std12',views.std12, name="std12"),
+    path('Btech',views.Btech, name="Btech"),
+    path('it',views.it, name="it"),
+    path('sem1it',views.sem1it, name="sem1it"),
+    path('sem2it',views.sem2it, name="sem2it"),
+    path('sem3it',views.sem3it, name="sem3it"),
+]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from ast import Return
+import imp
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    return render(request, "authentication/Main.html")
+
+def signup(request):
+
+    if request.method == "POST":
+        # username = request.POST.get('username')
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        myuser = User.objects.create(username, email, pass1)
+        myuser.firstname = fname
+        myuser.lastname = lname
+
+        myuser.save()
+
+        messages.success(request, "Your Account has been Successfuly Created")
+
+        return redirect('signin')
+
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    pass
+
+def aboutus(request):
+    return render(request, "authentication/aboutus.html")
+
+def std10(request):
+    return render(request, "authentication/std10.html")
+
+def std11(request):
+    return render(request, "authentication/std11.html")
+
+def std12(request):
+    return render(request, "authentication/std12.html")
+
+def Btech(request):
+    return render(request, "authentication/Btech.html")
+
+def it(request):
+    return render(request, "authentication/it.html")
+
+def sem1it(request):
+    return render(request, "authentication/sem1it.html")
+
+def sem2it(request):
+    return render(request, "authentication/sem2it.html")
+
+def sem3it(request):
+    return render(request, "authentication/sem3it.html")
