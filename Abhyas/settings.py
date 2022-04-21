0a87a781ca57089c841a120f5653878630fda95e
Navin Patil <navinpatilwork@gmail.com>
2022-04-22 02:06:18 +0530

    Enter

--- Abhyas/settings.py
+++ Abhyas/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,5 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    'static'
+    os.path.join(BASE_DIR,'static')
 ]
